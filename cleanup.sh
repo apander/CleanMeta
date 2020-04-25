1199eb4446d0e0724a1f6ddb8f19f88dd3bba9ed
Alasdair Anderson <alasdair.anderson@gmail.com>
2020-04-25 19:22:11 +0200

    1.0 version

--- cleanup.sh
+++ cleanup.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
-DIR="$1"
 
-# failsafe - fall back to current directory
-[ "$DIR" == "" ] && DIR="."
+setvars () {
+    DIR="$1"
+    # failsafe - fall back to current directory
+    [ "$DIR" == "" ] && DIR="."
+    # save and change IFS
+    OLDIFS=$IFS
+    IFS=$'\n'
+    # read all file name into an array
+    fileArray=($(find $DIR -type f))
+    # restore it
+    IFS=$OLDIFS
+    # get length of an array
+    tLen=${#fileArray[@]}
+    # set a leave switch
+    leavealone=''
+}
 
-# save and change IFS
-OLDIFS=$IFS
-IFS=$'\n'
+rename () {
+  #echo $newname
+  read -p "Are you sure? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    echo \"$filename\"" "\"$newname\"
+    mv "${filename}" "${newname}"
+  fi
+}
 
-# read all file name into an array
-fileArray=($(find $DIR -type f))
+getcutoff () {
+  echo What character should I cut from?
+  read cutposition
+  if [ -z $cutposition ]; then
+    #probably a single word film
+    newname=$displayname$fileext
+    echo "File will be renamed:"$newname
+  else
+    #cut the extra text
+    newname=${displayname::$cutposition}
+    newname=${newname//./ }
+    newname=$newname$fileext
+    echo "File will be renamed:"$newname
+  fi
+}
 
-# restore it
-IFS=$OLDIFS
-
-# get length of an array
-tLen=${#fileArray[@]}
-
-# use for loop read all filenames
-for (( i=0; i<${tLen}; i++ ));
-do
-  filename="${fileArray[$i]}"
-  echo $filename
-  length=${#filename}
-  #echo $length
-  line1=""
-  line2=""
-  for ((j=0; j<=$length; j++));
+displaycut () {
+  for ((j=1; j<=$length-6; j++));
   do
     if [ $j -lt 10 ]; then
       line1="0$line1"
@@ -38,4 +57,42 @@ do
   done
   echo $line1
   echo $line2
-done
+}
+
+main () {
+  #setup variable and array of files 
+  setvars
+
+  # use for loop read all filenames
+  for (( i=0; i<${tLen}; i++ ));
+  do
+    clear
+    #cut up the filename
+    filename="${fileArray[$i]}"
+    length=${#filename}
+    #remove windows file extension
+    displayname=${filename:2:$length-6}
+    #store the file extension
+    fileext=${filename:$length-4}
+    #clear out the display vars
+    line1=""
+    line2=""
+    echo $displayname
+
+    #show the character positions
+    displaycut
+    #prompt for a cut off point
+    getcutoff
+    #rename the file
+    rename
+  done
+  #clear
+
+  #Display the results
+  echo Updated Directory Results
+  echo =========================
+  echo 
+  ls -lh
+}
+
+main
